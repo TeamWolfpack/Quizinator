59be27eb1d5a17fe501bee7d46bd534457fbd2ea
Jim Kuczynski <kuczynskij@msoe.edu>
2016-09-28 14:04:46 -0500

    added stubs to the other two activities

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/Activities/GamePlayActivity.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/Activities/GamePlayActivity.java
@@ -2,6 +2,7 @@ package com.seniordesign.wolfpack.quizinator.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.seniordesign.wolfpack.quizinator.R;
 
@@ -11,5 +12,29 @@ public class GamePlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
+    }
+
+    public String onClick(View view){
+        return null;
+    }
+
+    private boolean addToGameStatsDB(){
+        return false;
+    }
+
+    private boolean initializeDB(){
+//        datasource = new ItemDataSource(this);
+//        datasource.open();
+        return true;
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
     }
 }
--- app/src/main/java/com/seniordesign/wolfpack/quizinator/Activities/NewGameSettingsActivity.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/Activities/NewGameSettingsActivity.java
@@ -2,6 +2,7 @@ package com.seniordesign.wolfpack.quizinator.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.seniordesign.wolfpack.quizinator.R;
 
@@ -11,5 +12,33 @@ public class NewGameSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game_settings);
+    }
+
+    public String onClick(View view){
+        return null;
+    }
+
+    public boolean loadRules(){
+        return false;
+    }
+
+    public boolean loadDeck(){
+        return false;
+    }
+
+    private boolean initializeDB(){
+//        datasource = new ItemDataSource(this);
+//        datasource.open();
+        return true;
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
     }
 }
