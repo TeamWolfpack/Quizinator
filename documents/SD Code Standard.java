e9e5d7b426a01580e03e83c54edf4660aae90aa5
Jim Kuczynski <kuczynskij@msoe.edu>
2016-09-28 13:17:28 -0500

    added documents folder and put in activities star uml file

---
+++ documents/SD Code Standard.java
@@ -0,0 +1,28 @@
+
+/**
+ *	Description of the class.
+ *	@creation 09/21/2016
+ */
+public class Example{
+	
+	String betterDescriptionThanAMethodVariable;
+	
+	//each date is the last time an author worked on method
+	//review is last person to review and the date it was reviewed
+	/*
+	 * @author kuczynskij (09/21/2016)
+	 * @author chuna (09/21/2016)
+	 * @author farrowc (09/21/2016)
+	 * @author leonardj (09/21/2016)
+	 * @review chuna (09/22/2016)
+	 */
+	 private Helper MethodNamesNeedToDescribeThePurpose(){
+		String methodNameIsShort;
+		for(int i = 0; i < 10; ++i){
+			//only use short variable names if they exist briefly
+		}
+		return new somethingThatCanBeTested;
+	 }
+}
+
+
