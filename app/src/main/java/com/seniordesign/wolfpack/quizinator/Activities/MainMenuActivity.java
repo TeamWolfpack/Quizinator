4e7a997a73f2fd717a2dbbcafa5ec2b6de27224b
Chris Farrow <farrowcguy@gmail.com>
2016-10-05 13:27:24 -0500

    Added comments to the main menu activity

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/Activities/MainMenuActivity.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/Activities/MainMenuActivity.java
@@ -3,8 +3,6 @@ package com.seniordesign.wolfpack.quizinator.Activities;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,13 +12,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 
 import com.seniordesign.wolfpack.quizinator.R;
 
+/**
+ * The main menu activity
+ * @creation 10/4/2016
+ */
 public class MainMenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    /**
+     * @author farrowc 10/4/2016
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +43,9 @@ public class MainMenuActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    /**
+     * @author farrowc 10/4/2016
+     */
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -49,6 +56,9 @@ public class MainMenuActivity extends AppCompatActivity
         }
     }
 
+    /**
+     * @author farrowc 10/4/2016
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -56,6 +66,9 @@ public class MainMenuActivity extends AppCompatActivity
         return true;
     }
 
+    /**
+     * @author farrowc 10/4/2016
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -71,6 +84,9 @@ public class MainMenuActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * @author farrowc 10/4/2016
+     */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -90,6 +106,9 @@ public class MainMenuActivity extends AppCompatActivity
         return true;
     }
 
+    /**
+     * @author farrowc 10/4/2016
+     */
     public void showGameSettings(View v){
         final Intent intent = new Intent(this, NewGameSettingsActivity.class);
         startActivity(intent);
