ae5fa4fee958fe2c392bd91dbdd776d036868b1c
unknown <leonardj@msoe.edu>
2016-10-09 14:51:55 -0500

    scroll to view in test seemed to work, adding it to the rest of the methods

--- app/src/androidTest/java/com/seniordesign/wolfpack/quizinator/GameSettingTests.java
+++ app/src/androidTest/java/com/seniordesign/wolfpack/quizinator/GameSettingTests.java
@@ -129,30 +129,30 @@ public class GameSettingTests {
     @Test
     public void validateCardMinuteInput_LowerBound() {
         onView(withId(R.id.card_minutes)).check(matches(withText("00")));
-        onView(withId(R.id.card_minutes)).perform(clearText(), typeText("-1"));
+        onView(withId(R.id.card_minutes)).perform(scrollTo(), clearText(), typeText("-1"));
         onView(withId(R.id.card_minutes)).check(matches(withText("1")));
     }
 
     @Test
     public void validateCardMinuteInput_UpperBound() {
         onView(withId(R.id.card_minutes)).check(matches(withText("00")));
-        onView(withId(R.id.card_minutes)).perform(clearText(), typeText("60"));
+        onView(withId(R.id.card_minutes)).perform(scrollTo(), clearText(), typeText("60"));
         onView(withId(R.id.card_minutes)).check(matches(withText("60")));
 
-        onView(withId(R.id.card_minutes)).perform(clearText(), typeText("61"));
+        onView(withId(R.id.card_minutes)).perform(scrollTo(), clearText(), typeText("61"));
         onView(withId(R.id.card_minutes)).check(matches(withText("6")));
     }
 
     @Test
     public void validateCardSecondInput_LowerBound() throws InterruptedException {
         onView(withId(R.id.card_seconds)).check(matches(withText("03")));
-        onView(withId(R.id.card_seconds)).perform(clearText(), typeText("1"));
+        onView(withId(R.id.card_seconds)).perform(scrollTo(), clearText(), typeText("1"));
         onView(withId(R.id.card_seconds)).check(matches(withText("1")));
 
-        onView(withId(R.id.card_seconds)).perform(clearText(), typeText("00"));
+        onView(withId(R.id.card_seconds)).perform(scrollTo(), clearText(), typeText("00"));
         onView(withId(R.id.card_seconds)).check(matches(withText("")));
 
-        onView(withId(R.id.card_seconds)).perform(clearText(), typeText("-1"));
+        onView(withId(R.id.card_seconds)).perform(scrollTo(), clearText(), typeText("-1"));
         onView(withId(R.id.card_seconds)).check(matches(withText("1")));
     }
 
@@ -169,19 +169,19 @@ public class GameSettingTests {
     @Test
     public void validateCardCountInput() {
         onView(withId(R.id.card_count)).check(matches(withText("10")));
-        onView(withId(R.id.card_count)).perform(clearText(), typeText("1"));
+        onView(withId(R.id.card_count)).perform(scrollTo(), clearText(), typeText("1"));
         onView(withId(R.id.card_count)).check(matches(withText("1")));
 
-        onView(withId(R.id.card_count)).perform(clearText(), typeText("00"));
+        onView(withId(R.id.card_count)).perform(scrollTo(), clearText(), typeText("00"));
         onView(withId(R.id.card_count)).check(matches(withText("")));
 
-        onView(withId(R.id.card_count)).perform(clearText(), typeText("-1"));
+        onView(withId(R.id.card_count)).perform(scrollTo(), clearText(), typeText("-1"));
         onView(withId(R.id.card_count)).check(matches(withText("1")));
 
-        onView(withId(R.id.card_count)).perform(clearText(), typeText("20"));
+        onView(withId(R.id.card_count)).perform(scrollTo(), clearText(), typeText("20"));
         onView(withId(R.id.card_count)).check(matches(withText("2")));
 
-        onView(withId(R.id.card_count)).perform(clearText(), typeText("10"));
+        onView(withId(R.id.card_count)).perform(scrollTo(), clearText(), typeText("10"));
         onView(withId(R.id.card_count)).check(matches(withText("10")));
     }
 
@@ -192,17 +192,21 @@ public class GameSettingTests {
         onView(withId(R.id.game_semicolon)).check(matches(isDisplayed()));
         onView(withId(R.id.game_seconds)).check(matches(isDisplayed()));
 
+        onView(withId(R.id.card_label)).perform(scrollTo());
         onView(withId(R.id.card_label)).check(matches(isDisplayed()));
         onView(withId(R.id.card_minutes)).check(matches(isDisplayed()));
         onView(withId(R.id.card_semicolon)).check(matches(isDisplayed()));
         onView(withId(R.id.card_seconds)).check(matches(isDisplayed()));
 
+        onView(withId(R.id.card_count_label)).perform(scrollTo());
         onView(withId(R.id.card_count_label)).check(matches(isDisplayed()));
         onView(withId(R.id.card_count)).check(matches(isDisplayed()));
 
+        onView(withId(R.id.card_type_spinner)).perform(scrollTo());
         onView(withId(R.id.card_type_spinner)).check(matches(isDisplayed()));
         onView(withId(R.id.card_type)).check(matches(isDisplayed()));
 
+        onView(withId(R.id.new_game)).perform(scrollTo());
         onView(withId(R.id.new_game)).check(matches(isDisplayed()));
     }
 
