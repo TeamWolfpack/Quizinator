2a78b7a7c55c0057940b6406444735c3772533d1
unknown <leonardj@msoe.edu>
2016-10-10 15:04:29 -0500

    re-enabled notifications from travis and fixed up tests to run locally

--- app/src/androidTest/java/com/seniordesign/wolfpack/quizinator/GameSettingTests.java
+++ app/src/androidTest/java/com/seniordesign/wolfpack/quizinator/GameSettingTests.java
@@ -85,7 +85,6 @@ public class GameSettingTests {
 
     @Test
     public void validateSpinnerOptions() {
-        onView(withId(R.id.card_type_spinner)).perform(scrollTo());
         String[] cardTypes = new String[] {"True/False", "Multiple Choice", "Both"};
         for (int index = 0; index < cardTypes.length; index++) {
             String type = cardTypes[index];
@@ -132,59 +131,59 @@ public class GameSettingTests {
     @Test
     public void validateCardMinuteInput_LowerBound() {
         onView(withId(R.id.card_minutes)).check(matches(withText("00")));
-        onView(withId(R.id.card_minutes)).perform(scrollTo(), clearText(), typeText("-1"));
+        onView(withId(R.id.card_minutes)).perform(clearText(), typeText("-1"));
         onView(withId(R.id.card_minutes)).check(matches(withText("1")));
     }
 
     @Test
     public void validateCardMinuteInput_UpperBound() {
         onView(withId(R.id.card_minutes)).check(matches(withText("00")));
-        onView(withId(R.id.card_minutes)).perform(scrollTo(), clearText(), typeText("60"));
+        onView(withId(R.id.card_minutes)).perform(clearText(), typeText("60"));
         onView(withId(R.id.card_minutes)).check(matches(withText("60")));
 
-        onView(withId(R.id.card_minutes)).perform(scrollTo(), clearText(), typeText("61"));
+        onView(withId(R.id.card_minutes)).perform(clearText(), typeText("61"));
         onView(withId(R.id.card_minutes)).check(matches(withText("6")));
     }
 
     @Test
     public void validateCardSecondInput_LowerBound() throws InterruptedException {
         onView(withId(R.id.card_seconds)).check(matches(withText("03")));
-        onView(withId(R.id.card_seconds)).perform(scrollTo(), clearText(), typeText("1"));
+        onView(withId(R.id.card_seconds)).perform(clearText(), typeText("1"));
         onView(withId(R.id.card_seconds)).check(matches(withText("1")));
 
-        onView(withId(R.id.card_seconds)).perform(scrollTo(), clearText(), typeText("00"));
+        onView(withId(R.id.card_seconds)).perform(clearText(), typeText("00"));
         onView(withId(R.id.card_seconds)).check(matches(withText("")));
 
-        onView(withId(R.id.card_seconds)).perform(scrollTo(), clearText(), typeText("-1"));
+        onView(withId(R.id.card_seconds)).perform(clearText(), typeText("-1"));
         onView(withId(R.id.card_seconds)).check(matches(withText("1")));
     }
 
     @Test
     public void validateCardSecondInput_UpperBound() {
         onView(withId(R.id.card_seconds)).check(matches(withText("03")));
-        onView(withId(R.id.card_seconds)).perform(scrollTo(), clearText(), typeText("60"));
+        onView(withId(R.id.card_seconds)).perform(clearText(), typeText("60"));
         onView(withId(R.id.card_seconds)).check(matches(withText("60")));
 
-        onView(withId(R.id.card_seconds)).perform(scrollTo(), clearText(), typeText("61"));
+        onView(withId(R.id.card_seconds)).perform(clearText(), typeText("61"));
         onView(withId(R.id.card_seconds)).check(matches(withText("6")));
     }
 
     @Test
     public void validateCardCountInput() {
         onView(withId(R.id.card_count)).check(matches(withText("10")));
-        onView(withId(R.id.card_count)).perform(scrollTo(), clearText(), typeText("1"));
+        onView(withId(R.id.card_count)).perform(clearText(), typeText("1"));
         onView(withId(R.id.card_count)).check(matches(withText("1")));
 
-        onView(withId(R.id.card_count)).perform(scrollTo(), clearText(), typeText("00"));
+        onView(withId(R.id.card_count)).perform(clearText(), typeText("00"));
         onView(withId(R.id.card_count)).check(matches(withText("")));
 
-        onView(withId(R.id.card_count)).perform(scrollTo(), clearText(), typeText("-1"));
+        onView(withId(R.id.card_count)).perform(clearText(), typeText("-1"));
         onView(withId(R.id.card_count)).check(matches(withText("1")));
 
-        onView(withId(R.id.card_count)).perform(scrollTo(), clearText(), typeText("20"));
+        onView(withId(R.id.card_count)).perform(clearText(), typeText("20"));
         onView(withId(R.id.card_count)).check(matches(withText("2")));
 
-        onView(withId(R.id.card_count)).perform(scrollTo(), clearText(), typeText("10"));
+        onView(withId(R.id.card_count)).perform(clearText(), typeText("10"));
         onView(withId(R.id.card_count)).check(matches(withText("10")));
     }
 
@@ -195,21 +194,17 @@ public class GameSettingTests {
         onView(withId(R.id.game_semicolon)).check(matches(isDisplayed()));
         onView(withId(R.id.game_seconds)).check(matches(isDisplayed()));
 
-        onView(withId(R.id.card_label)).perform(scrollTo());
         onView(withId(R.id.card_label)).check(matches(isDisplayed()));
         onView(withId(R.id.card_minutes)).check(matches(isDisplayed()));
         onView(withId(R.id.card_semicolon)).check(matches(isDisplayed()));
         onView(withId(R.id.card_seconds)).check(matches(isDisplayed()));
 
-        onView(withId(R.id.card_count_label)).perform(scrollTo());
         onView(withId(R.id.card_count_label)).check(matches(isDisplayed()));
         onView(withId(R.id.card_count)).check(matches(isDisplayed()));
 
-        onView(withId(R.id.card_type_spinner)).perform(scrollTo());
         onView(withId(R.id.card_type_spinner)).check(matches(isDisplayed()));
         onView(withId(R.id.card_type)).check(matches(isDisplayed()));
 
-        onView(withId(R.id.new_game)).perform(scrollTo());
         onView(withId(R.id.new_game)).check(matches(isDisplayed()));
     }
 
