514b5b048d6170a0e96f9317048aee1b7b90dde6
Leonard <leonardj@msoe.edu>
2016-10-11 15:01:26 -0500

    added a few more tests to make the coverage over 70%

--- app/src/androidTest/java/com/seniordesign/wolfpack/quizinator/MainMenuTests.java
+++ app/src/androidTest/java/com/seniordesign/wolfpack/quizinator/MainMenuTests.java
@@ -3,10 +3,12 @@ package com.seniordesign.wolfpack.quizinator;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -18,16 +20,21 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerActions.close;
 import static android.support.test.espresso.contrib.DrawerActions.open;
 import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.instanceOf;
@@ -79,12 +86,19 @@ public class MainMenuTests {
 
     @Test
     public void validateDrawerBehavior() {
-        onView(withId(R.id.drawer_layout)).check(matches(isClosed(Gravity.LEFT)));
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed(Gravity.START)));
         onView(withId(R.id.drawer_layout)).perform(open());
 
         pressBack();
 
-        onView(withId(R.id.drawer_layout)).check(matches(isClosed(Gravity.LEFT)));
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed(Gravity.START)));
+    }
+
+    @Test
+    public void validateToolbarOptions() {
+        openActionBarOverflowOrOptionsMenu(mActivityRule.getActivity());
+        onView(withText(R.string.action_settings)).check(matches(isDisplayed()));
+        onView(withText(R.string.action_settings)).perform(click());
     }
 
     @Test
@@ -96,5 +110,6 @@ public class MainMenuTests {
 
         String expectedItem = InstrumentationRegistry.getTargetContext().getString(R.string.quiz_bowl_rules);
         onView(withText(expectedItem)).check(matches(isDisplayed()));
+        onView(withText(expectedItem)).perform(click());
     }
 }
