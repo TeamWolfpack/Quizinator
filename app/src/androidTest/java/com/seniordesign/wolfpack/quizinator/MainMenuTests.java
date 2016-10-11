9b7551b1d13f6b76379439213fa399b01410e796
unknown <leonardj@msoe.edu>
2016-10-10 21:07:19 -0500

    added more tests to main menu, end of game, and filtering. Were at 81% coverage right now

--- app/src/androidTest/java/com/seniordesign/wolfpack/quizinator/MainMenuTests.java
+++ app/src/androidTest/java/com/seniordesign/wolfpack/quizinator/MainMenuTests.java
@@ -2,8 +2,11 @@ package com.seniordesign.wolfpack.quizinator;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.Gravity;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -16,7 +19,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerActions.open;
+import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -58,9 +66,35 @@ public class MainMenuTests {
     }
 
     @Test
-    public void TitleTest() {
-        //Test checks the toolbar title
+    public void titleTest() {
         onView(allOf(instanceOf(TextView.class), withParent(withId(R.id.toolbar))))
                 .check(matches(withText("Main Menu")));
+    }
+
+    @Test
+    public void validateNewGameIntent() {
+        onView(withId(R.id.newGameButton)).perform(click());
+        onView(withId(R.id.game_label)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void validateDrawerBehavior() {
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed(Gravity.LEFT)));
+        onView(withId(R.id.drawer_layout)).perform(open());
+
+        pressBack();
+
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed(Gravity.LEFT)));
+    }
+
+    @Test
+    public void validateDrawerItems() {
+        onView(withId(R.id.drawer_layout)).perform(open());
+
+        String expectedTitle = InstrumentationRegistry.getTargetContext().getString(R.string.app_name);
+        onView(withId(R.id.header_title)).check(matches(withText(expectedTitle)));
+
+        String expectedItem = InstrumentationRegistry.getTargetContext().getString(R.string.quiz_bowl_rules);
+        onView(withText(expectedItem)).check(matches(isDisplayed()));
     }
 }
