68d18e945bae5a2a2407704d4b98c576b0b8fabf
Jim <kuczynskij@msoe.edu>
2016-10-16 20:11:55 -0500

    added update method to highscore dao

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/Database/HighScore/HighScoresDataSource.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/Database/HighScore/HighScoresDataSource.java
@@ -82,6 +82,18 @@ public class HighScoresDataSource {
     }
 
     /*
+     * @author kuczynskij (10/16/2016)
+     */
+    public void updateHighScore(long id, HighScores hs) {
+        ContentValues cv = new ContentValues();
+            cv.put(HighScoresSQLiteHelper.COLUMN_DECKNAME, hs.getDeckName());
+            cv.put(HighScoresSQLiteHelper.COLUMN_BESTTIME, hs.getBestTime());
+            cv.put(HighScoresSQLiteHelper.COLUMN_BESTSCORE, hs.getBestScore());
+        String where = HighScoresSQLiteHelper.COLUMN_ID + " = " + id;
+        database.update(HighScoresSQLiteHelper.TABLE_HIGHSCORES, cv, where, null);
+    }
+
+    /*
      * @author kuczynskij (10/10/2016)
      */
     public boolean deleteHighScore(HighScores scores) {
