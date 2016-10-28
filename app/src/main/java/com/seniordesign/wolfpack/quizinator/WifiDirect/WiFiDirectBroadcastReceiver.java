e934bbb87d15b9ee7771b938a03f1e262a115587
aaronchun7 <aaronchun7@gmail.com>
2016-10-27 20:58:49 -0500

    added to android manifest

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/WiFiDirectBroadcastReceiver.java
@@ -3,6 +3,7 @@ package com.seniordesign.wolfpack.quizinator.WifiDirect;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 
 /**
  * A BroadcastReceiver that notifies of important wifi p2p events.
