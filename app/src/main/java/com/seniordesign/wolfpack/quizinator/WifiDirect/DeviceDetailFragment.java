6a6f8faafb2d711b674ee7f71253c775c6030a28
Leonard <leonardj@msoe.edu>
2016-10-28 10:29:13 -0500

    comment out unused code. Pretty sure the developer forgot to delete it from the previous example project

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/DeviceDetailFragment.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/DeviceDetailFragment.java
@@ -218,90 +218,96 @@ public class DeviceDetailFragment extends Fragment {
         this.getView().setVisibility(View.GONE);
     }
 
-    /**
-     * A simple server socket that accepts connection and writes some data on
-     * the stream.
+    /*
+     * leonardj
+     *
+     * 99% sure this code is from wifi-direct-demo and they forgot to delete it.
      */
-    public static class FileServerAsyncTask extends AsyncTask<Void, Void, String> {
 
-        private Context context;
-        private TextView statusText;
-
-        /**
-         * @param context
-         * @param statusText
-         */
-        public FileServerAsyncTask(Context context, View statusText) {
-            this.context = context;
-            this.statusText = (TextView) statusText;
-        }
-
-        @Override
-        protected String doInBackground(Void... params) {
-            try {
-                ServerSocket serverSocket = new ServerSocket(8988);
-
-                Socket client = serverSocket.accept();
-
-                final File f = new File(Environment.getExternalStorageDirectory() + "/"
-                        + context.getPackageName() + "/wifip2pshared-" + System.currentTimeMillis()
-                        + ".jpg");
-
-                File dirs = new File(f.getParent());
-                if (!dirs.exists())
-                    dirs.mkdirs();
-                f.createNewFile();
-
-                InputStream inputstream = client.getInputStream();
-                copyFile(inputstream, new FileOutputStream(f));
-                serverSocket.close();
-                return f.getAbsolutePath();
-            } catch (IOException e) {
-                return null;
-            }
-        }
-
-        /*
-         * (non-Javadoc)
-         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
-         */
-        @Override
-        protected void onPostExecute(String result) {
-            if (result != null) {
-                statusText.setText("File copied - " + result);
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.setDataAndType(Uri.parse("file://" + result), "image/*");
-                context.startActivity(intent);
-            }
-
-        }
-
-        /*
-         * (non-Javadoc)
-         * @see android.os.AsyncTask#onPreExecute()
-         */
-        @Override
-        protected void onPreExecute() {
-            statusText.setText("Opening a server socket");
-        }
-
-    }
-
-    public static boolean copyFile(InputStream inputStream, OutputStream out) {
-        byte buf[] = new byte[1024];
-        int len;
-        try {
-            while ((len = inputStream.read(buf)) != -1) {
-                out.write(buf, 0, len);
-
-            }
-            out.close();
-            inputStream.close();
-        } catch (IOException e) {
-            return false;
-        }
-        return true;
-    }
+//    /**
+//     * A simple server socket that accepts connection and writes some data on
+//     * the stream.
+//     */
+//    public static class FileServerAsyncTask extends AsyncTask<Void, Void, String> {
+//
+//        private Context context;
+//        private TextView statusText;
+//
+//        /**
+//         * @param context
+//         * @param statusText
+//         */
+//        public FileServerAsyncTask(Context context, View statusText) {
+//            this.context = context;
+//            this.statusText = (TextView) statusText;
+//        }
+//
+//        @Override
+//        protected String doInBackground(Void... params) {
+//            try {
+//                ServerSocket serverSocket = new ServerSocket(8988);
+//
+//                Socket client = serverSocket.accept();
+//
+//                final File f = new File(Environment.getExternalStorageDirectory() + "/"
+//                        + context.getPackageName() + "/wifip2pshared-" + System.currentTimeMillis()
+//                        + ".jpg");
+//
+//                File dirs = new File(f.getParent());
+//                if (!dirs.exists())
+//                    dirs.mkdirs();
+//                f.createNewFile();
+//
+//                InputStream inputstream = client.getInputStream();
+//                copyFile(inputstream, new FileOutputStream(f));
+//                serverSocket.close();
+//                return f.getAbsolutePath();
+//            } catch (IOException e) {
+//                return null;
+//            }
+//        }
+//
+//        /*
+//         * (non-Javadoc)
+//         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+//         */
+//        @Override
+//        protected void onPostExecute(String result) {
+//            if (result != null) {
+//                statusText.setText("File copied - " + result);
+//                Intent intent = new Intent();
+//                intent.setAction(Intent.ACTION_VIEW);
+//                intent.setDataAndType(Uri.parse("file://" + result), "image/*");
+//                context.startActivity(intent);
+//            }
+//
+//        }
+//
+//        /*
+//         * (non-Javadoc)
+//         * @see android.os.AsyncTask#onPreExecute()
+//         */
+//        @Override
+//        protected void onPreExecute() {
+//            statusText.setText("Opening a server socket");
+//        }
+//
+//    }
+//
+//    public static boolean copyFile(InputStream inputStream, OutputStream out) {
+//        byte buf[] = new byte[1024];
+//        int len;
+//        try {
+//            while ((len = inputStream.read(buf)) != -1) {
+//                out.write(buf, 0, len);
+//
+//            }
+//            out.close();
+//            inputStream.close();
+//        } catch (IOException e) {
+//            return false;
+//        }
+//        return true;
+//    }
 
 }
