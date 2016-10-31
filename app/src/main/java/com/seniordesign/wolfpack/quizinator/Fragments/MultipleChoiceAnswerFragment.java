5675456dafefacf7cd53832071ec5065f3dd1ebd
Chris Farrow <farrowcguy@gmail.com>
2016-10-31 00:59:44 -0500

    Multiple choice card is looking good!  The fragment loads answers properly and checks them properly

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/Fragments/MultipleChoiceAnswerFragment.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/Fragments/MultipleChoiceAnswerFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.seniordesign.wolfpack.quizinator.R;
 
@@ -27,6 +28,8 @@ public class MultipleChoiceAnswerFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private String[] choiceButtons = new String[4];
 
     private OnFragmentInteractionListener mListener;
 
@@ -65,15 +68,31 @@ public class MultipleChoiceAnswerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_multiple_choice_answer, container, false);
+        View view = inflater.inflate(R.layout.fragment_multiple_choice_answer, container, false);
+        ((Button) view.findViewById(R.id.choiceAButton)).setText(choiceButtons[0]);
+        ((Button) view.findViewById(R.id.choiceBButton)).setText(choiceButtons[1]);
+        ((Button) view.findViewById(R.id.choiceCButton)).setText(choiceButtons[2]);
+        ((Button) view.findViewById(R.id.choiceDButton)).setText(choiceButtons[3]);
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    public boolean setChoiceA(String choice){
+        choiceButtons[0] = choice;
+        return true;
     }
+    public boolean setChoiceB(String choice){
+        choiceButtons[1] = choice;
+        return true;
+    }
+    public boolean setChoiceC(String choice){
+        choiceButtons[2] = choice;
+        return true;
+    }
+    public boolean setChoiceD(String choice){
+        choiceButtons[3] = choice;
+        return true;
+    }
+
 
     @Override
     public void onAttach(Context context) {
@@ -104,6 +123,6 @@ public class MultipleChoiceAnswerFragment extends Fragment {
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onFragmentInteraction(String answer);
     }
 }
