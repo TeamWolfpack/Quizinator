8b50c71e13ac57aa1769718370be594de5828723
Jim <kuczynskij@msoe.edu>
2017-01-10 16:38:21 -0600

    minor code cleanup

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/Fragments/DeviceListFragment.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/Fragments/DeviceListFragment.java
@@ -120,11 +120,6 @@ public class DeviceListFragment extends ListFragment {
             deviceNameTextView.setText(this.myDevice.deviceName);
             deviceDetailsTextView.setText(R.string.wifi_direct_disabled);
         }
-
-        else{
-            //deviceNameTextView.setText(this.myDevice.deviceName);
-            deviceDetailsTextView.setText("Unavailable");
-        }
     }
 
     /**
