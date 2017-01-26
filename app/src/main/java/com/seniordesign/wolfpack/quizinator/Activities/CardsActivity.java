7f0ec7d6a341998baef481db655aaca2dcccad8b
Jim <kuczynskij@msoe.edu>
2017-01-26 09:56:21 -0600

    spacing

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/Activities/CardsActivity.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/Activities/CardsActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
