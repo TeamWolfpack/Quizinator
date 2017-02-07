c36594535b7755716a1841036c634a72fd8ab28b
Jim <kuczynskij@msoe.edu>
2017-02-07 14:49:59 -0600

    cleaned message codes in wifi direct folder

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/MessageCodes.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/MessageCodes.java
@@ -2,26 +2,20 @@ package com.seniordesign.wolfpack.quizinator.WifiDirect;
 
 public final class MessageCodes {
 
-    public static final String PACKAGE_NAME = MessageCodes.class.getPackage().getName();
+    static final String PACKAGE_NAME =
+            MessageCodes.class.getPackage().getName();
 
-    public static final int MSG_NULL = 0;
-    public static final int MSG_STARTSERVER = 1001;
-    public static final int MSG_STARTCLIENT = 1002;
-    public static final int MSG_CONNECT = 1003;
-    public static final int MSG_DISCONNECT = 1004;   // p2p disconnect
-    public static final int MSG_PUSHOUT_DATA = 1005;
-    public static final int MSG_NEW_CLIENT = 1006;
-    public static final int MSG_FINISH_CONNECT = 1007;
-    public static final int MSG_PULLIN_DATA = 1008;
-    public static final int MSG_REGISTER_ACTIVITY = 1009;
+    static final int MSG_NULL = 0;
+    static final int MSG_STARTSERVER = 1001;
+    static final int MSG_STARTCLIENT = 1002;
+    static final int MSG_PUSHOUT_DATA = 1005;
+    static final int MSG_NEW_CLIENT = 1006;
+    static final int MSG_FINISH_CONNECT = 1007;
+    static final int MSG_PULLIN_DATA = 1008;
+    static final int MSG_REGISTER_ACTIVITY = 1009;
 
-    public static final int MSG_SELECT_ERROR = 2001;
-    public static final int MSG_BROKEN_CONN = 2002;  // network disconnect
-
-    public static final int MSG_SIZE = 50;    // the lastest 50 messages
-    public static final String MSG_SENDER = "sender";
-    public static final String MSG_TIME = "time";
-    public static final String MSG_CONTENT = "msg";
+    static final int MSG_SELECT_ERROR = 2001;
+    static final int MSG_BROKEN_CONN = 2002;
 
     //gameplay messages
     public static final int MSG_PLAYER_READY_ACTIVITY = 3000;
@@ -33,7 +27,4 @@ public final class MessageCodes {
 
     //update peerlist
     public static final int MSG_DISCONNECT_FROM_ALL_PEERS = 4000;
-
-    //Testing messages
-    public static final int MSG_TEST_HI_JIMMY = -1;
 }
