44aea4f872cc8c940adeb75ee3dd2fa293380aaa
Jim <kuczynskij@msoe.edu>
2017-02-07 13:00:36 -0600

    cleaned up WorkHandler

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/WorkHandler.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/WorkHandler.java
@@ -5,58 +5,28 @@ import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Process;
 
-public final class WorkHandler {
+final class WorkHandler {
 
     private HandlerThread mHandlerThread;
     private Handler mHandler;
-    
-    public WorkHandler(String threadName) {
-        //mHandlerThread = new HandlerThread(threadName, Process.THREAD_PRIORITY_BACKGROUND);
+
+    WorkHandler(String threadName) {
         mHandlerThread = new HandlerThread(threadName,
                 Process.THREAD_PRIORITY_DEFAULT);
         mHandlerThread.start();
         mHandler = new Handler(mHandlerThread.getLooper());
     }
 
-    /*
-     * @author kuczynskij (10/26/2016)
-     */
-    public Handler getHandler() {
-        return mHandler;
+    Looper getLooper() {
+        return mHandler != null ? mHandler.getLooper() : null;
     }
 
-    /*
-     * @author kuczynskij (10/26/2016)
-     */
-    public Looper getLooper() {
-        Handler h = mHandler;
-        return h != null ? h.getLooper() : null;
-    }
-
-    /**
-     * Close the WorkHandler instance. Should be called when the
-     * WorkHandler is no longer needed.
-     */
-    /*
-     * @author kuczynskij (10/26/2016)
-     */
-    public void close() {
+    @Override
+    protected void finalize() {
         if (mHandler != null) {
             mHandlerThread.getLooper().quit();
             mHandlerThread.quit();
             mHandler = null;
-        }
-    }
-
-    /*
-     * @author kuczynskij (10/26/2016)
-     */
-    @Override
-    protected void finalize() {
-        //cleanup if needed and log the fact the WorkHandler
-        //instance was not properly closed.
-        if (mHandler != null) {
-            close();
         }
     }
 }
