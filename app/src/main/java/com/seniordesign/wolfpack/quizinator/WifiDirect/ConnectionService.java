7cb9968b05e9518776fb62fd9d07e8d1cb248279
Jim <kuczynskij@msoe.edu>
2017-02-26 22:11:25 -0600

    created MessageHandler class to clear out the ConnectionService class

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/ConnectionService.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/ConnectionService.java
@@ -1,8 +1,5 @@
 package com.seniordesign.wolfpack.quizinator.WifiDirect;
 
-import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.*;
-
-import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,27 +10,21 @@ import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.ChannelListener;
 import android.os.Binder;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
-import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 
 import com.google.gson.Gson;
 
 import com.seniordesign.wolfpack.quizinator.Activities.MainMenuActivity;
-import com.seniordesign.wolfpack.quizinator.Database.Card;
-import com.seniordesign.wolfpack.quizinator.Database.Rules;
-import com.seniordesign.wolfpack.quizinator.Messages.Answer;
-import com.seniordesign.wolfpack.quizinator.Messages.Confirmation;
 import com.seniordesign.wolfpack.quizinator.Messages.QuizMessage;
 
 public class ConnectionService extends Service implements ChannelListener {
 
     private static final String TAG = "ConnServ";
 
-    private static ConnectionService connectionService = null;
+    private static ConnectionService mInstance = null;
 
     private WorkHandler mWorkHandler;
     private MessageHandler mHandler;
@@ -41,19 +32,29 @@ public class ConnectionService extends Service implements ChannelListener {
     boolean retryChannel = false;
 
     WifiDirectApp wifiDirectApp;
-    MainMenuActivity mActivity;
+//    MainMenuActivity mActivity;
     ConnectionManager mConnMan;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (mInstance != null) {
+            return;
+        }
+        mInstance = this;
+        initializeConnectionService();
+    }
+
+    public static ConnectionService getInstance() {
+        return mInstance;
+    }
 
     private void initializeConnectionService() {
         Log.d(TAG, "initializeConnectionService");
 
-        if (connectionService != null) {
-            return;
-        }
-
-        connectionService = this;
         mWorkHandler = new WorkHandler(TAG);
-        mHandler = new MessageHandler(mWorkHandler.getLooper());
+        mHandler = new MessageHandler(mWorkHandler.getLooper(), mConnMan);
+        mConnMan = new ConnectionManager(this);
 
         wifiDirectApp = (WifiDirectApp) getApplication();
         wifiDirectApp.mP2pMan = (WifiP2pManager)
@@ -61,18 +62,6 @@ public class ConnectionService extends Service implements ChannelListener {
         wifiDirectApp.mP2pChannel =
                 wifiDirectApp.mP2pMan.initialize(this,
                         mWorkHandler.getLooper(), null);
-
-        mConnMan = new ConnectionManager(this);
-    }
-
-    public static ConnectionService getInstance() {
-        return connectionService;
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        initializeConnectionService();
     }
 
     @Override
@@ -102,11 +91,6 @@ public class ConnectionService extends Service implements ChannelListener {
         }
     }
 
-    private void sendMessageToUpDatePeerListFragment() {
-        if (wifiDirectApp.mHomeActivity != null)
-            wifiDirectApp.mHomeActivity.onConnectionInfoAvailable(wifiDirectApp.mP2pInfo);
-    }
-
     @Override
     public IBinder onBind(Intent arg0) {
         return new Binder();
@@ -116,100 +100,6 @@ public class ConnectionService extends Service implements ChannelListener {
         return mHandler;
     }
 
-    final class MessageHandler extends Handler {
-        MessageHandler(Looper looper) {
-            super(looper);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            processMessage(msg);
-        }
-    }
-
-    private void processMessage(Message msg) {
-        Log.d(TAG, "processMessage: message - " + msg.toString());
-        switch (msg.what) {
-            case MSG_NULL:
-                break;
-            case MSG_REGISTER_ACTIVITY:
-                onActivityRegister((MainMenuActivity) msg.obj, msg.arg1);
-                break;
-            case MSG_STARTSERVER:
-                if (mConnMan.startServerSelector() >= 0)
-                    sendMessageToUpDatePeerListFragment();
-                break;
-            case MSG_STARTCLIENT:
-                if (mConnMan.startClientSelector((String) msg.obj) >= 0)
-                    sendMessageToUpDatePeerListFragment();
-                break;
-            case MSG_NEW_CLIENT:
-                mConnMan.onNewClient((SocketChannel) msg.obj);
-                break;
-            case MSG_FINISH_CONNECT:
-                mConnMan.onFinishConnect((SocketChannel) msg.obj);
-                break;
-            case MSG_PULLIN_DATA:
-                onPullInData(msg.getData());
-                break;
-            case MSG_PUSHOUT_DATA:
-                onPushOutData((String) msg.obj);
-                break;
-            case MSG_SEND_RULES_ACTIVITY:
-                pushAllRulesOut((String) msg.obj);
-                break;
-            case MSG_SEND_CARD_ACTIVITY:
-                pushCardOut((String) msg.obj);
-                break;
-            case MSG_PLAYER_READY_ACTIVITY:
-                pushReadyOut((String) msg.obj);
-                break;
-            case MSG_SEND_ANSWER_ACTIVITY:
-                pushAnswerOut((String) msg.obj);
-                break;
-            case MSG_ANSWER_CONFIRMATION_ACTIVITY:
-                pushConfirmationOut((String) msg.obj);
-                break;
-            case MSG_END_OF_GAME_ACTIVITY:
-                pushEndOfGameOut((String) msg.obj);
-                break;
-            case MSG_SELECT_ERROR:
-                Log.d(TAG, "received an error related to the connection manager");
-                break;
-            case MSG_BROKEN_CONN:
-                mConnMan.onBrokenConnection((SocketChannel) msg.obj);
-                break;
-            case MSG_DISCONNECT_FROM_ALL_PEERS:
-                pushDisconnectAllPeersOut((String) msg.obj);
-                break;
-            default:
-                break;
-        }
-    }
-
-    /**
-     * Register the activity that uses this service.
-     */
-    private void onActivityRegister(MainMenuActivity activity,
-                                    int register) {
-        Log.d(TAG, "onActivityRegister: activity register " +
-                "itself to service : " + register);
-        if (register == 1)
-            mActivity = activity;
-        else
-            mActivity = null;
-    }
-
-    /**
-     * Handle data push out request.
-     *  If the sender is the server, pub to all client.
-     *  If the sender is client, only can send to the server.
-     */
-    private void onPushOutData(String data) {
-        Log.d(TAG, "onPushOutData : " + data);
-        mConnMan.pushOutData(data);
-    }
-
     public static boolean sendMessage(int code, String message) {
         Message result = getInstance().getHandler().obtainMessage();
             result.what = code;
@@ -217,108 +107,6 @@ public class ConnectionService extends Service implements ChannelListener {
         return getInstance().getHandler().sendMessage(result);
     }
 
-    private String createQuizMessage(int code, String message) {
-        return new Gson().toJson(new QuizMessage(code, message));
-    }
-
-    private void pushCardOut(String data){
-        Log.d(TAG, "pushCardOut: " + data);
-        mConnMan.pushOutData(createQuizMessage(MSG_SEND_CARD_ACTIVITY, data));
-    }
-
-    private void pushAllRulesOut(String data){
-        Log.d(TAG, "pushAllRulesOut: " + data);
-        mConnMan.pushOutData(createQuizMessage(MSG_SEND_RULES_ACTIVITY, data));
-    }
-
-    private void pushReadyOut(String data) {
-        Log.d(TAG, "pushReadyOut: " + data);
-        mConnMan.pushOutData(createQuizMessage(MSG_PLAYER_READY_ACTIVITY, data));
-    }
-
-    private void pushAnswerOut(String data) {
-        Log.d(TAG, "pushAnswerOut: " + data);
-        mConnMan.pushOutData(createQuizMessage(MSG_SEND_ANSWER_ACTIVITY, data));
-    }
-
-    private void pushConfirmationOut(String data) {
-        Log.d(TAG, "pushConfirmationOut: " + data);
-        Confirmation confirmation = new Gson().fromJson(data, Confirmation.class);
-        mConnMan.publishDataToSingleClient(
-                createQuizMessage(
-                    MSG_ANSWER_CONFIRMATION_ACTIVITY,
-                    String.valueOf(confirmation.getConfirmation())),
-                confirmation.getClientAddress());
-    }
-
-    private void pushEndOfGameOut(String data) {
-        Log.d(TAG, "pushEndOfGameOut: " + data);
-        mConnMan.pushOutData(createQuizMessage(MSG_END_OF_GAME_ACTIVITY, data));
-    }
-
-    private void pushDisconnectAllPeersOut(String data) {
-        Log.d(TAG, "pushDisconnectAllPeersOut: " + data);
-        mConnMan.pushOutData(createQuizMessage(MSG_DISCONNECT_FROM_ALL_PEERS, data));
-    }
-
-    private String onPullInData(Bundle b){
-        String data = b.getString("DATA");
-        Log.d(TAG, "onPullInData: received message - " + data);
-        Gson gson = new Gson();
-        List<QuizMessage> messages = parseInData(data);
-        for (QuizMessage msg: messages) {
-            String message = msg.getMessage();
-            switch(msg.getCode()){
-                case MSG_SEND_RULES_ACTIVITY:
-                    wifiDirectApp.mHomeActivity.startMultiplayerGamePlay(
-                            gson.fromJson(message, Rules.class));
-                    break;
-                case MSG_SEND_CARD_ACTIVITY:
-                    wifiDirectApp.mGameplayActivity.receivedNextCard(
-                            gson.fromJson(message, Card.class));
-                    break;
-                case MSG_PLAYER_READY_ACTIVITY:
-                    if (wifiDirectApp.mManageActivity != null)
-                        wifiDirectApp.mManageActivity.deviceIsReady(message);
-                    break;
-                case MSG_SEND_ANSWER_ACTIVITY:
-                    wifiDirectApp.mManageActivity.validateAnswer(
-                            gson.fromJson(message, Answer.class));
-                    break;
-                case MSG_ANSWER_CONFIRMATION_ACTIVITY:
-                    wifiDirectApp.mGameplayActivity.answerConfirmed(
-                            Boolean.parseBoolean(message));
-                    break;
-                case MSG_END_OF_GAME_ACTIVITY:
-                    wifiDirectApp.mGameplayActivity.endGamePlay(
-                            Long.parseLong(message));
-                    break;
-                case MSG_DISCONNECT_FROM_ALL_PEERS:
-                    if(wifiDirectApp.mHomeActivity != null){
-                        wifiDirectApp.mHomeActivity.disconnect();
-                        wifiDirectApp.mHomeActivity.finish();
-                    }
-                    break;
-            }
-        }
-        return data;
-    }
-
-    public List<QuizMessage> parseInData(String data) {
-        Log.d(TAG, "parseInData: " + data);
-        Gson gson = new Gson();
-        ArrayList<QuizMessage> messages = new ArrayList<>();
-        String[] chunks = data.split("\\}\\{");
-        for (int i = 0; i < chunks.length; i++) {
-            String chunk = chunks[i];
-            if (i < chunks.length - 1)
-                chunk += "}";
-            if (i > 0)
-                chunk = "{" + chunk;
-            messages.add(gson.fromJson(chunk, QuizMessage.class));
-        }
-        return messages;
-    }
 
     public static String getDeviceStatus(int deviceStatus) {
         switch (deviceStatus) {
---
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/MessageHandler.java
@@ -0,0 +1,210 @@
+package com.seniordesign.wolfpack.quizinator.WifiDirect;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.seniordesign.wolfpack.quizinator.Activities.MainMenuActivity;
+import com.seniordesign.wolfpack.quizinator.Database.Card;
+import com.seniordesign.wolfpack.quizinator.Database.Rules;
+import com.seniordesign.wolfpack.quizinator.Messages.Answer;
+import com.seniordesign.wolfpack.quizinator.Messages.Confirmation;
+import com.seniordesign.wolfpack.quizinator.Messages.QuizMessage;
+
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_ANSWER_CONFIRMATION_ACTIVITY;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_BROKEN_CONN;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_DISCONNECT_FROM_ALL_PEERS;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_END_OF_GAME_ACTIVITY;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_FINISH_CONNECT;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_NEW_CLIENT;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_NULL;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_PLAYER_READY_ACTIVITY;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_PULLIN_DATA;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_PUSHOUT_DATA;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_SELECT_ERROR;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_SEND_ANSWER_ACTIVITY;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_SEND_CARD_ACTIVITY;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_SEND_RULES_ACTIVITY;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_STARTCLIENT;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_STARTSERVER;
+import static com.seniordesign.wolfpack.quizinator.WifiDirect.MessageCodes.MSG_REGISTER_ACTIVITY;
+
+public class MessageHandler extends Handler {
+
+    private static final String TAG = "MsgHnd";
+
+    private ConnectionManager mConnMan;
+    private WifiDirectApp wifiDirectApp;
+
+    MainMenuActivity mActivity;
+
+    MessageHandler(Looper looper, ConnectionManager connMan) {
+        super(looper);
+        mConnMan = connMan;
+        wifiDirectApp = WifiDirectApp.getInstance();
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+        processMessage(msg);
+    }
+
+    private void processMessage(Message msg) {
+        Log.d(TAG, "processMessage: message - " + msg.toString());
+        switch (msg.what) {
+            case MSG_NULL:
+                break;
+            case MSG_REGISTER_ACTIVITY:
+                onActivityRegister((MainMenuActivity) msg.obj, msg.arg1);
+                break;
+            case MSG_STARTSERVER:
+                if (mConnMan.startServerSelector() >= 0)
+                    sendMessageToUpDatePeerListFragment();
+                break;
+            case MSG_STARTCLIENT:
+                if (mConnMan.startClientSelector((String) msg.obj) >= 0)
+                    sendMessageToUpDatePeerListFragment();
+                break;
+            case MSG_NEW_CLIENT:
+                mConnMan.onNewClient((SocketChannel) msg.obj);
+                break;
+            case MSG_FINISH_CONNECT:
+                mConnMan.onFinishConnect((SocketChannel) msg.obj);
+                break;
+            case MSG_PULLIN_DATA:
+                onPullInData(msg.getData());
+                break;
+            case MSG_PUSHOUT_DATA:
+                mConnMan.pushOutData((String) msg.obj);
+                break;
+            case MSG_SEND_RULES_ACTIVITY:
+                mConnMan.pushOutData(createQuizMessage(MSG_SEND_RULES_ACTIVITY, (String) msg.obj));
+                break;
+            case MSG_SEND_CARD_ACTIVITY:
+                mConnMan.pushOutData(createQuizMessage(MSG_SEND_CARD_ACTIVITY, (String) msg.obj));
+                break;
+            case MSG_PLAYER_READY_ACTIVITY:
+                mConnMan.pushOutData(createQuizMessage(MSG_PLAYER_READY_ACTIVITY, (String) msg.obj));
+                break;
+            case MSG_SEND_ANSWER_ACTIVITY:
+                mConnMan.pushOutData(createQuizMessage(MSG_SEND_ANSWER_ACTIVITY, (String) msg.obj));
+                break;
+            case MSG_ANSWER_CONFIRMATION_ACTIVITY:
+                pushConfirmationOut((String) msg.obj);
+                break;
+            case MSG_END_OF_GAME_ACTIVITY:
+                mConnMan.pushOutData(createQuizMessage(MSG_END_OF_GAME_ACTIVITY, (String) msg.obj));
+                break;
+            case MSG_SELECT_ERROR:
+                Log.d(TAG, "received an error related to the connection manager");
+                break;
+            case MSG_BROKEN_CONN:
+                mConnMan.onBrokenConnection((SocketChannel) msg.obj);
+                break;
+            case MSG_DISCONNECT_FROM_ALL_PEERS:
+                mConnMan.pushOutData(createQuizMessage(MSG_DISCONNECT_FROM_ALL_PEERS, (String) msg.obj));
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Register the activity that uses this service.
+     */
+    private void onActivityRegister(MainMenuActivity activity,
+                                    int register) {
+        Log.d(TAG, "onActivityRegister: activity register " +
+                "itself to service : " + register);
+        if (register == 1)
+            mActivity = activity;
+        else
+            mActivity = null;
+    }
+
+    private void sendMessageToUpDatePeerListFragment() {
+        if (wifiDirectApp.mHomeActivity != null)
+            wifiDirectApp.mHomeActivity.onConnectionInfoAvailable(wifiDirectApp.mP2pInfo);
+    }
+
+    private void pushConfirmationOut(String data) {
+        Log.d(TAG, "pushConfirmationOut: " + data);
+        Confirmation confirmation = new Gson().fromJson(data,
+                Confirmation.class);
+        mConnMan.publishDataToSingleClient(
+                createQuizMessage(
+                        MSG_ANSWER_CONFIRMATION_ACTIVITY,
+                        String.valueOf(confirmation.getConfirmation())),
+                confirmation.getClientAddress());
+    }
+
+    private String createQuizMessage(int code, String message) {
+        return new Gson().toJson(new QuizMessage(code, message));
+    }
+
+    private String onPullInData(Bundle b){
+        String data = b.getString("DATA");
+        Log.d(TAG, "onPullInData: received message - " + data);
+        Gson gson = new Gson();
+        List<QuizMessage> messages = parseInData(data);
+        for (QuizMessage msg: messages) {
+            String message = msg.getMessage();
+            switch(msg.getCode()){
+                case MSG_SEND_RULES_ACTIVITY:
+                    wifiDirectApp.mHomeActivity.startMultiplayerGamePlay(
+                            gson.fromJson(message, Rules.class));
+                    break;
+                case MSG_SEND_CARD_ACTIVITY:
+                    wifiDirectApp.mGameplayActivity.receivedNextCard(
+                            gson.fromJson(message, Card.class));
+                    break;
+                case MSG_PLAYER_READY_ACTIVITY:
+                    if (wifiDirectApp.mManageActivity != null)
+                        wifiDirectApp.mManageActivity.deviceIsReady(message);
+                    break;
+                case MSG_SEND_ANSWER_ACTIVITY:
+                    wifiDirectApp.mManageActivity.validateAnswer(
+                            gson.fromJson(message, Answer.class));
+                    break;
+                case MSG_ANSWER_CONFIRMATION_ACTIVITY:
+                    wifiDirectApp.mGameplayActivity.answerConfirmed(
+                            Boolean.parseBoolean(message));
+                    break;
+                case MSG_END_OF_GAME_ACTIVITY:
+                    wifiDirectApp.mGameplayActivity.endGamePlay(
+                            Long.parseLong(message));
+                    break;
+                case MSG_DISCONNECT_FROM_ALL_PEERS:
+                    if(wifiDirectApp.mHomeActivity != null){
+                        wifiDirectApp.mHomeActivity.disconnect();
+                        wifiDirectApp.mHomeActivity.finish();
+                    }
+                    break;
+            }
+        }
+        return data;
+    }
+
+    public List<QuizMessage> parseInData(String data) {
+        Log.d(TAG, "parseInData: " + data);
+        Gson gson = new Gson();
+        ArrayList<QuizMessage> messages = new ArrayList<>();
+        String[] chunks = data.split("\\}\\{");
+        for (int i = 0; i < chunks.length; i++) {
+            String chunk = chunks[i];
+            if (i < chunks.length - 1)
+                chunk += "}";
+            if (i > 0)
+                chunk = "{" + chunk;
+            messages.add(gson.fromJson(chunk, QuizMessage.class));
+        }
+        return messages;
+    }
+}
