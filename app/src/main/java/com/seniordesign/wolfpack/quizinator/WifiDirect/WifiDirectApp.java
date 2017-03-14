bce5add1fb276c562ab737285b85e9b9dc6b3268
Jim <kuczynskij@msoe.edu>
2017-03-14 14:22:59 -0500

    added invited devices to be pushed to the top of the peer list

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/WifiDirectApp.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/WifiDirect/WifiDirectApp.java
@@ -150,7 +150,8 @@ public class WifiDirectApp extends Application {
             Log.d(TAG, "onPeersAvailable: wifiDirectApp.mServer is false (CLIENT)");
             for (WifiP2pDevice device : mPeers) {
                 //Order connected device to the top of the list
-                if (device.isGroupOwner() && device.status == WifiP2pDevice.CONNECTED)
+                if (device.isGroupOwner() && (device.status == WifiP2pDevice.CONNECTED ||
+                        device.status == WifiP2pDevice.INVITED))
                     filteredPeers.add(0, device);
                 else if (device.isGroupOwner())
                     filteredPeers.add(device);
