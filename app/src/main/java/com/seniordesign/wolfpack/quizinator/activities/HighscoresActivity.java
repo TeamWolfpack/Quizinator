fe15b7b200b1a376f9bcb47e7837088adc76571f
Chris Farrow <farrowcguy@gmail.com>
2017-04-06 16:59:09 -0500

    Made the highscores activity and list

---
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/activities/HighscoresActivity.java
@@ -0,0 +1,13 @@
+package com.seniordesign.wolfpack.quizinator.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighscoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscores);
+    }
+}
