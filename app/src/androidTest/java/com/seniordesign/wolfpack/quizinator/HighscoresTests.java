aa59049135dc414e9f9eda58caf1ae69a8a36fd4
Leonard <leonardj@msoe.edu>
2017-04-07 13:25:32 -0500

    more tests

--- app/src/androidTest/java/com/seniordesign/wolfpack/quizinator/HighscoresTests.java
+++ app/src/androidTest/java/com/seniordesign/wolfpack/quizinator/HighscoresTests.java
@@ -5,8 +5,11 @@ import android.support.test.espresso.DataInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.WindowManager;
+import android.widget.ListView;
 
 import com.seniordesign.wolfpack.quizinator.activities.HighscoresActivity;
+import com.seniordesign.wolfpack.quizinator.adapters.HighScoreAdapter;
+import com.seniordesign.wolfpack.quizinator.database.Card;
 import com.seniordesign.wolfpack.quizinator.database.Deck;
 import com.seniordesign.wolfpack.quizinator.database.HighScores;
 import com.seniordesign.wolfpack.quizinator.database.QuizDataSource;
@@ -16,6 +19,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static android.support.test.espresso.Espresso.onData;
@@ -51,7 +55,16 @@ public class HighscoresTests {
 
     @Test
     public void checkHighscoreList() {
+        QuizDataSource dataSource = new QuizDataSource(InstrumentationRegistry.getTargetContext());
+        dataSource.open();
+        deleteAllHighscores(dataSource);
+        dataSource.createHighScore(1, 1000, 1);
+        updateListView(dataSource);
+
         onView(withId(R.id.list_of_highscores)).check(matches(isDisplayed()));
+
+        deleteAllHighscores(dataSource);
+        dataSource.close();
     }
 
     @Test
@@ -60,6 +73,7 @@ public class HighscoresTests {
         dataSource.open();
         deleteAllHighscores(dataSource);
         dataSource.createHighScore(1, 1000, 1);
+        updateListView(dataSource);
 
         Deck deck = dataSource.getDeckWithId(1);
         DataInteraction highsoreView = onData(anything())
@@ -74,9 +88,72 @@ public class HighscoresTests {
         dataSource.close();
     }
 
+    @Test
+    public void displayTimeCorrectly() {
+        QuizDataSource dataSource = new QuizDataSource(InstrumentationRegistry.getTargetContext());
+        dataSource.open();
+        deleteAllHighscores(dataSource);
+        dataSource.createHighScore(1, 603000, 1);
+        updateListView(dataSource);
+
+        DataInteraction highsoreView = onData(anything())
+                .inAdapterView(withId(R.id.list_of_highscores))
+                .atPosition(0); // atPosition is 0 indexed
+        highsoreView.onChildView(withId(R.id.array_adapter_hs_time)).check(matches(withText(containsString("10:03"))));
+
+        deleteAllHighscores(dataSource);
+        dataSource.close();
+    }
+
+    @Test
+    public void displayMultipleHighscores() {
+        QuizDataSource dataSource = new QuizDataSource(InstrumentationRegistry.getTargetContext());
+        dataSource.open();
+        deleteAllHighscores(dataSource);
+        dataSource.createDeck("Highscore", "", "", true, "Test", new ArrayList<Card>());
+        dataSource.createHighScore(1, 3000, 1);
+        dataSource.createHighScore(2, 3000, 1);
+        updateListView(dataSource);
+
+        Deck deck1 = dataSource.getDeckWithId(1);
+        Deck deck2 = dataSource.getDeckWithId(2);
+        DataInteraction defaultView = onData(anything())
+                .inAdapterView(withId(R.id.list_of_highscores))
+                .atPosition(0); // atPosition is 0 indexed
+        DataInteraction secondView = onData(anything())
+                .inAdapterView(withId(R.id.list_of_highscores))
+                .atPosition(1); // atPosition is 0 indexed
+        defaultView.onChildView(withId(R.id.array_adapter_hs_score)).check(matches(withText(containsString("1"))));
+        defaultView.onChildView(withId(R.id.array_adapter_hs_time)).check(matches(withText(containsString("0:03"))));
+        defaultView.onChildView(withId(R.id.array_adapter_hs_deck)).check(matches(withText(containsString(deck1.getDeckName()))));
+        secondView.onChildView(withId(R.id.array_adapter_hs_score)).check(matches(withText(containsString("1"))));
+        secondView.onChildView(withId(R.id.array_adapter_hs_time)).check(matches(withText(containsString("0:03"))));
+        secondView.onChildView(withId(R.id.array_adapter_hs_deck)).check(matches(withText(containsString(deck2.getDeckName()))));
+
+        deleteAllHighscores(dataSource);
+        dataSource.close();
+    }
+
     private void deleteAllHighscores(QuizDataSource dataSource) {
         List<HighScores> hsList = dataSource.getAllHighScores();
         for (HighScores hs: hsList)
             dataSource.deleteHighScore(hs);
+    }
+
+    private void updateListView(final QuizDataSource dataSource) {
+        try {
+            mActivityRule.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    ListView list = (ListView) mActivityRule.getActivity().findViewById(R.id.list_of_highscores);
+                    HighScoreAdapter adapter = (HighScoreAdapter) list.getAdapter();
+                    adapter.clear();
+                    adapter.addAll(dataSource.getAllHighScores());
+                    adapter.notifyDataSetChanged();
+                }
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
     }
 }
