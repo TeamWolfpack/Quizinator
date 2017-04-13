d4eb8c0ae9a6505c607799428622712db032fc13
aaronchun7 <aaronchun7@gmail.com>
2017-04-13 17:12:14 -0500

    finished adding the sorting feature for edit deck on the available cards

--- app/src/main/java/com/seniordesign/wolfpack/quizinator/activities/EditDeckActivity.java
+++ app/src/main/java/com/seniordesign/wolfpack/quizinator/activities/EditDeckActivity.java
@@ -6,17 +6,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.google.gson.Gson;
+import com.seniordesign.wolfpack.quizinator.Constants;
 import com.seniordesign.wolfpack.quizinator.adapters.CardAdapter;
 import com.seniordesign.wolfpack.quizinator.database.Card;
 import com.seniordesign.wolfpack.quizinator.database.Deck;
 import com.seniordesign.wolfpack.quizinator.database.QuizDataSource;
 import com.seniordesign.wolfpack.quizinator.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import io.apptik.widget.multiselectspinner.BaseMultiSelectSpinner;
+import io.apptik.widget.multiselectspinner.MultiSelectSpinner;
+
+import static com.seniordesign.wolfpack.quizinator.Constants.CARD_TYPES.FREE_RESPONSE;
+import static com.seniordesign.wolfpack.quizinator.Constants.CARD_TYPES.MULTIPLE_CHOICE;
+import static com.seniordesign.wolfpack.quizinator.Constants.CARD_TYPES.TRUE_FALSE;
+import static com.seniordesign.wolfpack.quizinator.Constants.CARD_TYPES.VERBAL_RESPONSE;
 
 public class EditDeckActivity extends AppCompatActivity  implements AdapterView.OnItemClickListener{
 
@@ -24,6 +37,17 @@ public class EditDeckActivity extends AppCompatActivity  implements AdapterView.
     List<Card> cardsAvailable;
     CardAdapter deckCardAdapter;
     CardAdapter totalCardAdapter;
+
+    private String sortBy;
+    private MultiSelectSpinner cardTypeSpinner;
+    private List<Constants.CARD_TYPES> selectedCardTypes;
+    private List<Constants.CARD_TYPES> cardTypes = new ArrayList<>(
+            Arrays.asList(
+                    TRUE_FALSE,
+                    MULTIPLE_CHOICE,
+                    FREE_RESPONSE,
+                    VERBAL_RESPONSE
+            ));
 
     QuizDataSource dataSource;
 
@@ -37,10 +61,16 @@ public class EditDeckActivity extends AppCompatActivity  implements AdapterView.
         deck = gson.fromJson(jsonDeck,Deck.class);
         setTitle("Edit: " + deck.getDeckName());
 
-        initializeDB();
-        cardsAvailable = dataSource.getAllCards();
+        cardTypeSpinner = (MultiSelectSpinner) findViewById(R.id.card_type_spinner);
+        selectedCardTypes = new ArrayList<>();
 
-        populateMenus(deck, cardsAvailable);
+        initializeDB();
+        initializeCardTypeSpinner();
+        initializeSortBySpinner();
+
+        //TODO possibly get rid of in future
+//        cardsAvailable = dataSource.getAllCards();
+        populateMenus(deck);
     }
 
     private boolean initializeDB(){
@@ -48,7 +78,54 @@ public class EditDeckActivity extends AppCompatActivity  implements AdapterView.
         return dataSource.open() && dataSource.open();
     }
 
-    private void populateMenus(Deck deck, List<Card> cardsAvailable){
+    private Boolean initializeCardTypeSpinner(){
+        ArrayAdapter<Constants.CARD_TYPES> cardTypeAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_multiple_choice, cardTypes);
+        cardTypeSpinner
+                .setListAdapter(cardTypeAdapter)
+                .setAllCheckedText(Constants.ALL_CARD_TYPES)
+                .setSelectAll(true)
+                .setMinSelectedItems(1)
+                .setListener(new BaseMultiSelectSpinner.MultiSpinnerListener() {
+                    @Override
+                    public void onItemsSelected(boolean[] selected) {
+                        selectedCardTypes.clear();
+                        for (int i = 0; i < selected.length; i++) {
+                            if (selected[i]) {
+                                try {
+                                    selectedCardTypes.add(cardTypes.get(i));
+                                } catch (Exception e) {
+                                    System.out.println(e.getMessage());
+                                }
+                            }
+                        }
+                        ArrayList<Constants.CARD_TYPES> chosenTypes = new ArrayList<>();
+                        for (int i = 0; i < selectedCardTypes.size(); i++) {
+                            chosenTypes.add(selectedCardTypes.get(i));
+                        }
+                        populateMenus(deck);
+                    }
+                });
+        return true;
+    }
+
+    private void initializeSortBySpinner() {
+        Spinner sortBySpinner = (Spinner) findViewById(R.id.card_sort_spinner);
+        sortBySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                sortBy = parent.getSelectedItem().toString();
+                populateMenus(deck);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do Nothing
+            }
+        });
+    }
+
+    private void populateMenus(Deck deck){
         EditText deckName = (EditText) findViewById(R.id.edit_deck_name);
         deckName.setText(deck.getDeckName());
         final ListView deckCardList = (ListView)findViewById(R.id.cards_in_deck);
@@ -58,7 +135,8 @@ public class EditDeckActivity extends AppCompatActivity  implements AdapterView.
         deckCardList.setOnItemClickListener(this);
         final ListView totalCardList = (ListView)findViewById(R.id.cards_in_database);
         totalCardAdapter = new CardAdapter(this,
-                android.R.layout.simple_list_item_1, cardsAvailable);
+                android.R.layout.simple_list_item_1,
+                dataSource.filterCards(selectedCardTypes, sortBy));
         totalCardList.setAdapter(totalCardAdapter);
         totalCardList.setOnItemClickListener(this);
     }
